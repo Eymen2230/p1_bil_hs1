eb81b0e368f7c4ec973b50705ee2983b4ab6d97e
Eymen2230 <155750928+Eymen2230@users.noreply.github.com>
2024-01-05 15:21:13 +0000

    ekmek

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # print("merhaba dünya\nMerhaba Dünya\nMerhaba Dünya\n")
 # print("Merhaba"+" "+"Şahin")
-print(input("Merhaba, adın nedir?"))
+# print(input("Merhaba, adın nedir?"))
+print("Merhaba " + input("Merhaba,Adın Nedir?") + "!")
