4edad06dd290b8726243c71b7c7cf667722ab2e8
Eymen2230 <155750928+Eymen2230@users.noreply.github.com>
2024-01-05 16:32:41 +0000

    ekmek

--- main.py
+++ main.py
@@ -3,5 +3,47 @@
 # print(input("Merhaba, adın nedir?"))
 # print("Merhaba " + input("Merhaba,Adın Nedir?") + "!")
 # print(len(input("Merhaba, Adın Nedir?")))
-name = input("Merhaba Adın Nedir?")
-print(name)mbnb
+# name = input("Merhaba Adın Nedir?")
+# print(name)
+# name = "Şahin"
+# print(name)
+# name = "Taşın"
+# print(name)
+# print(len(input("Merhaba, Adın Nedir?")))
+# isim = input("Merhaba, Adın Nedir?")
+# uzunluk = len(isim)
+# print(uzunluk)
+# Sayi1 = input("1inci Sayıyı Giriniz: ")
+# Sayi2 = input("2nci Sayıyı Giriniz: ")
+# Sayi3 = Sayi1
+# Sayi1 = Sayi2
+# Sayi2 = Sayi3
+# print("Sayı1 : " + Sayi1)
+# print("Sayi2 : " + Sayi2)
+# Sehir = input("En Son Hangi Şehre Gittiniz?")
+# hayvan = input("Evcil hayvanınızın adı nedir?")
+# print("Grup adı: " + " " + Sehir + " " + hayvan)
+# Sayi1 = input("1inci Sayıyı giriniz: ")
+# Sayi2 = input("2nci Sayıyı Giriniz: ")
+# toplam = int(Sayi1) + int(Sayi2)
+# print("Toplam = " + str(toplam))
+# kisa = input("Kısa kenarı giriniz:")
+# uzun = input("Uzun kenarı giriniz:")
+# cevre = (kisa + uzun) * 2
+# alan = kisa * uzun
+# print("Dikdörtgenin çevresi: " + str(cevre))
+# print("Dikdörtgenin Alanı: " + str(alan))
+# yaricap = input("Yarıçapı giriniz: ")
+# yukseklik = input("Yüksekliği giriniz: ")
+# pi = 3.14
+# tabanAlani = pi *yaricap * yaricap
+# tabanCevresi = 2 * pi *yaricap
+# yanalAlan = yukseklik* tabanCevresi
+# toplamAlan = yanalAlan + (2 * tabanAlani)
+# hacim = tabanAlani * yukseklik
+yaricap = input("Yarıçapı giriniz: ")
+pi = 3.14
+cevre = 2 * pi * yaricap
+hacim = pi * yaricap * yaricap
+print("Dairenin Hacmi: " + str(hacim))
+print("Dairenin Çevresi: " + str(cevre))
