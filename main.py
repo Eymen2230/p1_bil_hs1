eda9740850958890db944029817815654c89d1c1
Eymen2230 <155750928+Eymen2230@users.noreply.github.com>
2024-01-08 16:58:19 +0000

    ekmek

--- main.py
+++ main.py
@@ -54,3 +54,80 @@
 # print(3.12345)
 # print(True)
 # print(False)
+#uzunluk = len(input("Merhaba, adın ne?"))
+#uzunluk_str = str(uzunluk)
+#print("İsminde " + uzunluk_str + " karakter var")
+#fl = float(1.322)
+#print(type(fl))
+#intt = int(232)
+#print(type(intt))
+#st = str(123123)
+#print(type(st))
+#print(100 + float(15.23))
+#print(str(122 ) + str(324))
+#ikihanelisayi = str(input("İki haneli bir sayı giriniz!"))
+#toplam = int(ikihanelisayi[0]) + int(ikihanelisayi[1])
+#print("Toplam: " + str(toplam))
+#boy  = float(input("Boyunuzu giriniz (m): "))
+#kilo = float(input("Kilonuzu giriniz (kg):"))
+#vki = int(kilo/boy**2)
+#print("Vücut Kitle İndeksiniz: " + str(vki))
+#print(8/3)
+#print(type(8/3))
+#print(8//3)
+#print(type(8//3))
+#result = 8/4
+#result /= 2
+#print(result)
+#sayi = 5
+#sayi +=1
+#print(sayi)
+#sayi = sayi+1
+#print(sayi)
+#print("Sayı = " +str(sayi)) 
+#print(f"Sayı = {sayi}")
+#yas = input("Kaç yaşındasın?")
+#yas_int = int(yas)
+#ay = yas_int * 12
+#hafta = yas_int * 52
+#gun = yas_int * 365
+#print(f"{ay} aylıksın")
+#print(f"{hafta} haftalıksın")
+#print(f"{gun} günlüksün")
+#print("Bahşiş hesaplayıcıya hoşgeldiniz!")
+#hesap = float(input("Hesap ne geldi?"))
+#kisiSayisi = int(input("Kaç kişisiniz?"))
+#oran = float(input("Bahşiş oranı ne olsun?"))
+#toplam = hesap + (hesap*oran/100)
+#kisiBasi = toplam /kisiSayisi
+#print(f"Kişibaşı ödemeniz gereken tutar = {kisiBasi} TL")
+#print("Lunaparka hoşgeldiniz!")
+#print("Bilet fiyatı yetişkin 10 TL. 12 yaşından küçükler 5 TL")
+#boy = int(input("Lütfen boyunuzu giriniz!(cm)"))
+#yas = int(input("Lütfen yaşınızı giriniz."))
+#biletfiyati = 0
+#if boy > 80 :
+#    if(        biletfiyati = 10
+#    print(f"Çarpışan Arabalara binebilirsiniz! Fiyat {biletfiyati} TL")
+#elif boy > 80 and boy < 120:
+#    if(yas < 12):
+    #    biletfiyati = 5
+   # else:
+  #      biletfiyati = 10
+ #   print(f"Lunapark Hız Trenine binebilirsiniz! Fiyat {biletfiyati} TL")
+#elif boy > 120 and boy < 140:
+    #if(yas < 12):
+      #  biletfiyati = 5 yas < 12):
+     #   biletfiyati = 5
+    #else:
+   # else:
+  #      biletfiyati = 10
+ #   print(f"Gondola binebilirsiniz! Fiyat {biletfiyati} TL")
+#elif boy > 140:
+    #if(yas < 12):
+    #    biletfiyati = 5
+   # else:
+  #      biletfiyati = 10
+ #   print(f"Kamikazeye binebilirsiniz! Fiyat {biletfiyati} TL")
+#else:
+ #   print("Lunaparkta herhangi bir araca binmek için boyunuz yetmiyor!")
